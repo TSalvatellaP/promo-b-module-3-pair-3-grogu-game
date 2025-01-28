5e799dded9f5adc4881e755e9b2e9a2f830b60a5
Patricia <patriciaparreirasainz@gmail.com>
2025-01-28 11:31:12 +0100

    grogu component and some fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,50 @@
 import "../styles/App.scss"
+import {useState} from 'react';
 import Header from "./Header";
 import Board from "./Board";
 
 function App() {
-  const groguPosition = [];
-  const frog = ["🐸", "🐸", "🐸"];
-  const cookie = ["🍪", "🍪", "🍪"];
-  const egg = ["🥚","🥚","🥚",]; 
-  let gameStatus = '';
+  // 1. cambiar las cinco a variables de estado (nombre, función, useState)
+  // 2. la función de roll dice, ajustar con las variables de estado
+  // 3. el groguPosition no es un array. El valor inicial es 0
+  // el board recibe groguPosition con props
+
+  // const [groguPosition, setGroguPosition] = 0; // 0 a 6
+  // const frog = ["🐸", "🐸", "🐸"];
+  // const cookie = ["🍪", "🍪", "🍪"];
+  // const egg = ["🥚","🥚","🥚",];
+  // let gameStatus = '';
+
+  let [groguPosition, setGroguPosition] = useState(0);
+  const [frogs, setFrog] = useState(["🐸", "🐸", "🐸"]);
+  const [cookies, setCookie] = useState(["🍪", "🍪", "🍪"]);
+  const [eggs, setEgg] = useState(["🥚","🥚","🥚",]);
+  let [gameStatus, setGameStatus] = useState("Lanza el dado");
 
   // esta función se queda en APP y se lleva al componente dice por props y luego lifting para recoger el estado
-  function rollDice() {
+  function rollDice(event) {
+    event.preventDefault();
     const dice = Math.ceil(Math.random() * 4)
-      if(dice === 4 && groguPosition.length > 0){
-     
+      if(dice === 4){
+        setGroguPosition = groguPosition + 1;
       // se mueve grogu
       gameStatus = "¡Grogu ha avanzado una casilla! 😱"
-    } else if (dice === 3 && frog.length > 0){
-      frog.splice (0,1);
+    } else if (dice === 3 && frogs.length > 0){
+      frogs.splice (0,1);
       // si es 1, 2 o 3, una mercancía se eliminará de su lista
       // asignar cada numero a una mercancia y quitar una si el array es mayor que 0
       gameStatus = "Se ha descargado una ranita"
-    } else if (dice === 2 && cookie.length > 0){
-      cookie.splice (0,1);
+    } else if (dice === 2 && cookies.length > 0){
+      cookies.splice (0,1);
       gameStatus = "Se ha descargado una galleta"
     
-    }else if (dice === 1 && egg.length > 0){
-    egg.splice (0,1);
+    }else if (dice === 1 && eggs.length > 0){
+    eggs.splice (0,1);
     gameStatus = "Se ha descargado un huevo"
     } else {
       gameStatus = "Tira de nuevo el dado"
     }
   }
-  rollDice();
-
-  
 
 
   return (
@@ -42,27 +52,27 @@ function App() {
     <div className="page">
       <Header/>
     <main className="page">
-      <Board/>
+      <Board groguPosition={groguPosition}/>
 
       <section className="sectionDice">
-        <button className="dice">Lanzar Dado</button>
-        <div className="game-status">{gameStatus}</div>
+        <button className="dice" onClick={rollDice()}>Lanzar Dado</button>
+        <div className="game-status">{setGameStatus}</div>
       </section>
 
       <section className="goods-container">
-        <div className="goods-item">{cookie[0]}</div>
-        <div className="goods-item">{cookie[1]}</div>
-        <div className="goods-item">{cookie[2]}</div>
+        <div className="goods-item">{cookies[0]}</div>
+        <div className="goods-item">{cookies[1]}</div>
+        <div className="goods-item">{cookies[2]}</div>
       </section>
       <section className="goods-container">
-        <div className="goods-item">{egg[0]}</div>
-        <div className="goods-item">{egg[1]}</div>
-        <div className="goods-item">{egg[2]}</div>
+        <div className="goods-item">{eggs[0]}</div>
+        <div className="goods-item">{eggs[1]}</div>
+        <div className="goods-item">{eggs[2]}</div>
       </section>
       <section className="goods-container">
-        <div className="goods-item">{frog[0]}</div>
-        <div className="goods-item">{frog[1]}</div>
-        <div className="goods-item">{frog[2]}</div>
+        <div className="goods-item">{frogs[0]}</div>
+        <div className="goods-item">{frogs[1]}</div>
+        <div className="goods-item">{frogs[2]}</div>
       </section>
 
       <section>
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,20 +1,19 @@
 import "../styles/Board.scss"
 import Grogu from "./Grogu";
 
-function Board() {
-// si recibe 4, mover a grogu de sitio
+function Board({groguPosition}) {
+
+  const arrayCells = Array(7).fill(null);
+
+  const listCells = arrayCells.map(
+    (cell, index) => {
+      return <div key={index} className="cell">{index === groguPosition ? <Grogu/> : null}</div>
+    }
+  )
 
   return (
     <section className="board">
-        {/* <div className="cell">{groguPosition[0]}
-            <Grogu/>
-        </div>
-        <div className="cell">{groguPosition[1]}</div>
-        <div className="cell">{groguPosition[2]}</div>
-        <div className="cell">{groguPosition[3]}</div>
-        <div className="cell">{groguPosition[4]}</div>
-        <div className="cell">{groguPosition[5]}</div>
-        <div className="cell">{groguPosition[6]}</div> */}
+      {listCells}
       </section>
   )
 }
